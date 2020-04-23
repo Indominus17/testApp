bdb0f1b01feb860f8040ba95f793d602009845d5
unknown <indominous1702@gmail.com>
2020-04-23 23:43:46 +0530

    title test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Helmet } from 'react-helmet'
 import './App.css';
 
 function App() {
   return (
+    <>
+    <Helmet>
+          <title>{ TITLE }</title>
+    </Helmet>
+    </>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
